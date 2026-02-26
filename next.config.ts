384178f237ad98a90fbf2ad19f18988a8b17bfd0
Anousack <64178877+Anousack789@users.noreply.github.com>
2026-02-26 16:41:23 +0700

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  experimental: {
+    workerThreads: false,
+    cpus: 1,
+  },
+  images: {
+    remotePatterns:
+      process.env.NODE_ENV === "production"
+        ? [
+            {
+              protocol: "https",
+              hostname: "example.com",
+            },
+          ]
+        : [
+            {
+              protocol: "http",
+              hostname: "localhost",
+            },
+          ],
+  },
 };
 
 export default nextConfig;
